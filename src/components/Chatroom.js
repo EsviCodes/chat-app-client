ed7a95c62d48f1f16b9910472360e035d8b6fbd7
EsviCodes <esther.js.vis@gmail.com>
2019-10-25 12:15:08 +0200

    Created constants file which stores the url of the database which we can use easily in other files

--- src/components/Chatroom.js
+++ src/components/Chatroom.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
+import url from "../constants";
 
 export default class Chatroom extends Component {
+  source = new EventSource(`${url}/stream`);
+
+  componentDidMount() {
+    console.log("Chat Room Component says hi"); // works
+    console.log("URL is:", url);
+    console.log("source", this.source);
+
+    this.source.onmessage = event => {
+      console.log("Got a message!", event);
+    };
+  }
+
   render() {
     return (
       <div>
-        <h1>Hello form the Chat Room</h1>
+        <h3>Hello from the Chat Room</h3>
       </div>
     );
   }
